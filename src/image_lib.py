fd86cf9911d0f8caa0cbaf1c41a72461a1a64272
vectominist <harrychang1017@gmail.com>
2020-05-04 18:47:07 +0800

    update py files

---
+++ src/image_lib.py
@@ -0,0 +1,48 @@
+import numpy as np
+from skimage import io, filters, color, transform, feature, exposure, external
+import matplotlib.pyplot as plt
+
+# Basic Operations
+def normalize(image):
+    image -= np.min(image)
+    image = image / np.max(image)
+    return image
+
+def thresholding(image, threshold):
+    return (image > threshold).astype(np.float64)
+
+def resize(image, shape_0, shape_1):
+    return transform.resize(image, (shape_0, shape_1))
+
+# Basic IO
+def read_image(filename, as_gray=False):
+    if filename.split('.')[-1] == 'tif':
+        image = external.tifffile.imread(filename)
+    else:
+        image = io.imread(filename, as_gray=as_gray) # numpy array
+    # color image : M x N x 3
+    # gray image  : M x N
+    # M : height
+    # N : width
+    if image.shape[-1] == 4:
+        image = color.rgba2rgb(image)
+    if image.ndim == 2:
+        image = color.gray2rgb(image)
+    return image
+
+def show_image(image, mode=None):
+    if mode is None:
+        if image.shape[2] == 1:
+            image = np.concatenate((image, image, image), axis=2)
+        plt.imshow(image)
+    elif mode == 'gray':
+        plt.imshow(image, cmap=plt.cm.gray)
+    plt.show()
+    # plt.clf()
+
+def save_image(filename, image, norm=True):
+    if norm:
+        image = normalize(image.astype(np.float64))
+    image *= 255
+    io.imsave(filename, image.astype(np.uint8))
+
