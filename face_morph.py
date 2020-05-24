57068b1980d3bc5172112251da5bd19916a6a13f
vectominist <harrychang1017@gmail.com>
2020-05-25 07:59:26 +0800

    add feature extraction into pipeline

--- face_morph.py
+++ face_morph.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from src.image_lib import resize_to_same, read_image, show_image, show_arrows
 from src.morphing_np import Morphing
+from src.feat_extract import FaceFeatureExtractor
 
 class FaceImageMorphing:
     '''
@@ -9,12 +10,13 @@ class FaceImageMorphing:
         including preprocessing, face feature extraction, and
         image morphing.
     '''
-    def __init__(self, a=0.1, b=1.0, p=0.5):
+    def __init__(self, a=0.1, b=1.0, p=0.5, predictor='shape_predictor_68_face_landmarks.dat'):
         # TODO: feature extraction
         self.Morph = Morphing(a, b, p)
+        self.FaceFeatureExtractor = FaceFeatureExtractor(predictor)
         raise NotImplementedError
     
-    def FaceMorph2D(self, img_1, img_2, ratio):
+    def FaceMorph2D(self, img_1_path, img_2_path, ratio):
         '''
             Combination of all steps of face image morphing.
             img_1 : input image 1
@@ -24,10 +26,12 @@ class FaceImageMorphing:
                     1.0 means the output image will be img_2
         '''
         raise NotImplementedError
-        # Image prepocessing (filtering/resize)
-        # TODO
 
         # Feature extraction
+        shape_1, img_1 = self.FaceFeatureExtractor.ExtractFeature(img_1_path)
+        shape_2, img_2 = self.FaceFeatureExtractor.ExtractFeature(img_2_path)
+        
+        # Process feature
         # TODO
         P1, Q1 = None, None
         P2, Q2 = None, None
